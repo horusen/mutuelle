9ce6601e7ea6dac696c688884eac39402c5dcb5e
horusen <kane.babacar@protonmail.com>
2022-05-28 11:40:54 +0000

    stage 1

---
+++ src/app/unamusc/unamusc.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UnamuscComponent } from './unamusc.component';
+import { MutuelCreateComponent } from '../mutuel-create/mutuel-create.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { RecapitulatifComponent } from '../recapitulatif/recapitulatif.component';
+import { MutuelleListComponent } from '../mutuelle-list/mutuelle-list.component';
+import { MutuelShowComponent } from '../mutuel-show/mutuel-show.component';
+import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from 'src/shared/shared.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UnamuscComponent,
+    children: [
+      {
+        path: 'mutuelles',
+        loadChildren: () =>
+          import('./../mutuelles/mutuelles.module').then(
+            (module) => module.MutuellesModule
+          ),
+      },
+      {
+        path: 'enregistrement-couts',
+        loadChildren: () =>
+          import('./../enregistrement-couts/enregistrement-couts.module').then(
+            (module) => module.EnregistrementCoutsModule
+          ),
+      },
+      {
+        path: 'enregistrement-prestations',
+        loadChildren: () =>
+          import(
+            './../enregistrement-prestations/enregistrement-prestations.module'
+          ).then((module) => module.EnregistrementPrestationsModule),
+      },
+
+      {
+        path: 'configurations',
+        loadChildren: () =>
+          import('./../configuration/configuration.module').then(
+            (module) => module.ConfigurationModule
+          ),
+      },
+    ],
+  },
+];
+
+@NgModule({
+  declarations: [
+    UnamuscComponent,
+    MutuelCreateComponent,
+    DashboardComponent,
+    RecapitulatifComponent,
+    MutuelleListComponent,
+    MutuelShowComponent,
+  ],
+  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class UnamuscModule {}
